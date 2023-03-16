d13984cff5dcc919d28852ed58dad520322ec64e
fistaco <fiske_schijlen@hotmail.com>
2023-03-16 18:51:54 +0100

    Implement Path Sum solution through recursive DFS.

--- PathSum/Program.cs
+++ PathSum/Program.cs
@@ -6,7 +6,38 @@
         {
             string treeString = Console.ReadLine();
             Node root = Node.ParseRootFromTreeString(treeString);
-            root.PrintTree();
+            int targetSum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(HasPathSum(root, targetSum));
+        }
+
+        public static bool HasPathSum(Node root, int targetSum)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+            return NodeHasPathSum(root, root.Value, targetSum);
+        }
+
+        public static bool NodeHasPathSum(Node? node, int? sum, int targetSum)
+        {
+            if (node.Left == null && node.Right == null)
+            {
+                return sum == targetSum;
+            }
+
+            if (node.Left == null)
+            {
+                return NodeHasPathSum(node.Right, sum + node.Right.Value, targetSum);
+            }
+
+            if (node.Right == null)
+            {
+                return NodeHasPathSum(node.Left, sum + node.Left.Value, targetSum);
+            }
+
+            return NodeHasPathSum(node.Left, sum + node.Left.Value, targetSum) || NodeHasPathSum(node.Right, sum + node.Right.Value, targetSum);
         }
     }
 }
