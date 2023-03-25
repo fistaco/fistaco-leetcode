90af8a38ba5335222ab6d2d00d0437fbf739aba0
fistaco <fiske_schijlen@hotmail.com>
2023-03-25 13:08:35 +0100

    Implement O(n^2) solution for the Two Sum problem.

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] TwoSum(int[] nums, int target)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = 0; j < nums.Length; j++)
+        {
+            if (i != j && nums[i] + nums[j] == target)
+            {
+                return new int[] {i, j};
+            }
+        }
+    }
+
+    return new int[2];
+}
+
+// Example: nums = [2,7,11,15], target = 9, output - [0,1]
+int[] nums = new int[] {2, 7, 11, 15};
+int target = 9;
+
+int[] two_sum_output = TwoSum(nums, target);
